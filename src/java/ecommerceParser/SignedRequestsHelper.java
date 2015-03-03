e44b9a35744f932b25ead1d54734ded27c9c957d
asemenov <semenov@goodwix.ru>
2015-03-03 19:51:40 +0300

    change

--- src/java/ecommerceParser/SignedRequestsHelper.java
+++ src/java/ecommerceParser/SignedRequestsHelper.java
@@ -45,7 +45,6 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
-
 /**
  * This class contains all the logic for signing requests
  * to the Amazon Product Advertising API.

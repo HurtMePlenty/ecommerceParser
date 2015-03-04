bf10229c391f0d35bb6d58603de43f80efe1b430
asemenov <semenov@goodwix.ru>
2015-03-04 20:44:12 +0300

    change

--- src/main/java/ecommerceParser/EntryPoint.java
+++ src/main/java/ecommerceParser/EntryPoint.java
@@ -1,3 +1,5 @@
+package ecommerceParser;
+
 import ecommerceParser.priceFinderModule.PriceFinder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -5,10 +7,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class EntryPoint {
 
     public static void main(String[] args) {
-        System.out.println("started");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         PriceFinder priceFinder = context.getBean("priceFinder", PriceFinder.class);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("--------------------------------");
+        System.out.println();
+        System.out.println("started");
+
         priceFinder.process();
         if (System.console() != null) {
             System.out.println("Successfully finished. Press enter to exit...");
